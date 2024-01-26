c20af37fa7824bb74a8c5f998a47f9bc1d593c60
de019050 <patric.stracke@sanofi.com>
2024-01-26 17:24:51 +0100

    Update kform_verification.R
    added new if structure

--- ISO3951Verification/R/kform_verification.R
+++ ISO3951Verification/R/kform_verification.R
@@ -15,17 +15,17 @@ ISO_KFORMTest <- read_csv("../DATA/ISO_KFORMTest.csv")
 # Create Test Input Data
 #
 ISO_KFORM_Input<-ISO_KFORMTest %>%
-  group_by(Charge,LSL,USL,kValue) %>% 
+  group_by(Charge,LSL,USL,kValue) %>%
   summarise(n=n(),value=mean(Value,na.rm=T), std=sd(Value,na.rm=T))
 
 
-# New k-form function 
+# New k-form function
 # bases on tibble containing all neccessary informations
 # input only one tibble
 
 # k_form<-function(Data){
-#   
-#   
+#
+#
 #   # itime
 #   k_u_itime=ifelse((data$itime_usl-data$mitime)/data$sitime-data$kfac<0,"FALSE","TRUE")
 #   k_u_itime_val=(data$itime_usl-data$mitime)/data$sitime-data$kfac
@@ -35,11 +35,11 @@ ISO_KFORM_Input<-ISO_KFORMTest %>%
 #   # npos
 #   k_l_npos=ifelse((data$mnpos-data$npos_usl)/data$snpos-data$kfac<0,"FALSE","TRUE")
 #   k_l_npos_val=(data$mnpos-data$npos_usl)/data$snpos-data$kfac
-#   
-#   
+#
+#
 #   #return(cbind(k_u_itime,k_u_itime_val+k, k,k_l_dose,k_l_dose_val+k,k,k_l_npos,k_l_npos_val+k, k))
 #   return(cbind(data$charge,data$mat_bez,k_u_itime,k_u_itime_val,k_l_dose,k_l_dose_val,k_l_npos,k_l_npos_val, data$kfac))
-#   
+#
 # }
 
 
@@ -49,11 +49,16 @@ myFunction <- function(input1, input2, input3 = NULL, input4 = NULL, input5) {
   if (missing(input1) || is.na(input1) || missing(input2) || is.na(input2) || missing(input5) || is.na(input5)) {
     stop("Inputs 1, 2, and 5 are mandatory and cannot be NA.")
   }
-  
+
   # Determine which option to use based on the presence and value of optional inputs
   input3_present = !missing(input3) && !is.na(input3)
   input4_present = !missing(input4) && !is.na(input4)
-  
+
+  # Check if input4 is larger than input3 when both are present
+  if (input3_present && input4_present && input4 <= input3) {
+    stop("Input4 must be larger than Input3.")
+  }
+
   if (!input3_present && !input4_present) {
     stop("At least one of input 3 or input 4 is required.")
   } else if (input4_present && !input3_present) {
@@ -73,36 +78,37 @@ myFunction <- function(input1, input2, input3 = NULL, input4 = NULL, input5) {
 
 
 
+
 k_form_ISO<-function(MeanValue,STD,LSL,USL,kFac){
 # Function to calculate k-form acceptance criteria
 # for one-sided values
-    if (LSL == 'NA') { 
+    if (LSL == 'NA') {
       QU= (USL-MeanValue)/STD
       #print(round(QU,digits=3))
       if (QU >= kFac){
-        cat('The Lot meets the acceptability criterion.criterion. QU =',round(QU,digits=3),'is geq than kfac =',kFac)  
+        cat('The Lot meets the acceptability criterion.criterion. QU =',round(QU,digits=3),'is geq than kfac =',kFac)
       } else if (QU < kFac){
-        cat('The Lot does not meet the acceptability criterion. QU =',round(QU,digits=3),'is less than kfac =',kFac)  
+        cat('The Lot does not meet the acceptability criterion. QU =',round(QU,digits=3),'is less than kfac =',kFac)
       } else {
-        cat('Check inputs')  
+        cat('Check inputs')
       }
-      
+
       } else if (USL == 'NA') {
       QL= (MeanValue-LSL)/STD
       #print(round(QL,digits = 3))
       if (QL >= kFac){
-        cat('The Lot meets the acceptability criterion. QL =',round(QL,digits=3),'is geq than kfac =',kFac)  
+        cat('The Lot meets the acceptability criterion. QL =',round(QL,digits=3),'is geq than kfac =',kFac)
       } else if (QL < kFac){
-        cat('The Lot does not meet the acceptability criterion. QL =',round(QL,digits=3),'is less than kfac =',kFac)  
+        cat('The Lot does not meet the acceptability criterion. QL =',round(QL,digits=3),'is less than kfac =',kFac)
       } else {
-        cat('Check inputs')  
+        cat('Check inputs')
       }
       } else if  (USL!= 'NA' ||LSL!= 'NA') {
       print("Two limits! Use p-form!")
     } else {
     print("Wrong inputs")
-    }      
-  
+    }
+
 }
 
 # Input Data from both examples in Chapter16.2
