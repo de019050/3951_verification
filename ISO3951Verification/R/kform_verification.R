de5a70d1e85222d839e8796d37c7560e170660c9
de019050 <99798828+de019050@users.noreply.github.com>
2024-01-29 12:40:01 +0100

    Update kform_verification.R
    added code for 2 onesided examples

--- ISO3951Verification/R/kform_verification.R
+++ ISO3951Verification/R/kform_verification.R
@@ -2,13 +2,13 @@ library(readr)
 library(dplyr)
 
 getwd()
-setwd("/home/de019050/R/3951_verification/CODE")
+setwd(getwd())
 
 # Allreflex Data until 25012024
-ALLREFLEX <- read_csv("../DATA/ALLREFLEX.csv")
+ALLREFLEX <- read_csv("DATA/ALLREFLEX.csv")
 
 # Kform Examples from Chapter 16.2 of ISO 3951-2
-ISO_KFORMTest <- read_csv("../DATA/ISO_KFORMTest.csv")
+ISO_KFORMTest <- read_csv("DATA/ISO_KFORMTest.csv")
 
 
 #
@@ -45,9 +45,15 @@ ISO_KFORM_Input<-ISO_KFORMTest %>%
 
 # Define the function with five parameters
 myFunction <- function(input1, input2, input3 = NULL, input4 = NULL, input5) {
-  # Check if mandatory inputs are missing or NA
+  MeanValue<-input1
+  STD<-input2
+  LSL<-input3
+  USL<-input4
+  kFac<-input5
+
+    # Check if mandatory inputs are missing or NA
   if (missing(input1) || is.na(input1) || missing(input2) || is.na(input2) || missing(input5) || is.na(input5)) {
-    stop("Inputs 1, 2, and 5 are mandatory and cannot be NA.")
+    stop("Mean Value, Standard Deviation, and k-Factor are mandatory and cannot be NA.")
   }
 
   # Determine which option to use based on the presence and value of optional inputs
@@ -56,27 +62,44 @@ myFunction <- function(input1, input2, input3 = NULL, input4 = NULL, input5) {
 
   # Check if input4 is larger than input3 when both are present
   if (input3_present && input4_present && input4 <= input3) {
-    stop("Input4 must be larger than Input3.")
+      print(input3)
+      print(input4)
+      stop("USL must be larger than LSL.")
   }
 
   if (!input3_present && !input4_present) {
-    stop("At least one of input 3 or input 4 is required.")
+    stop("At least one of LSL or USL is required.")
   } else if (input4_present && !input3_present) {
-    # Proceed with option 1
+    QU= (USL-MeanValue)/STD
+    #print(round(QU,digits=3))
+    if (QU >= kFac){
+      cat('The Lot meets the acceptability criterion.criterion. QU =',round(QU,digits=3),'is geq than kfac =',kFac)
+    } else if (QU < kFac){
+      cat('The Lot does not meet the acceptability criterion. QU =',round(QU,digits=3),'is less than kfac =',kFac)
+    } else {
+      cat('QU Check inputs')
+    }
     return("Option 1 activated")
   } else if (input3_present && !input4_present) {
-    # Proceed with option 2
+    QL= (MeanValue-LSL)/STD
+    #print(round(QL,digits = 3))
+    if (QL >= kFac){
+      cat('The Lot meets the acceptability criterion. QL =',round(QL,digits=3),'is geq than kfac =',kFac)
+    } else if (QL < kFac){
+      cat('The Lot does not meet the acceptability criterion. QL =',round(QL,digits=3),'is less than kfac =',kFac)
+    } else {
+      cat('Check inputs')
+    }
     return("Option 2 activated")
   } else {
     # Proceed with option 3
+    print("Two limits! Use p-form!")
     return("Option 3 activated")
   }
 }
 
 # Example usage
 # myFunction(input1, input2, input3, input4, input5)
-
-
 
 
 k_form_ISO<-function(MeanValue,STD,LSL,USL,kFac){
@@ -92,7 +115,6 @@ k_form_ISO<-function(MeanValue,STD,LSL,USL,kFac){
       } else {
         cat('Check inputs')
       }
-
       } else if (USL == 'NA') {
       QL= (MeanValue-LSL)/STD
       #print(round(QL,digits = 3))
@@ -115,5 +137,8 @@ k_form_ISO<-function(MeanValue,STD,LSL,USL,kFac){
 ISO_KFORM_Input
 # Example1
 k_form_ISO(54.615385,3.3301267,'NA',60,1.426)
+myFunction(54.615385,3.3301267,NA,60,1.426)
+
 # Example2
 k_form_ISO(6.551,0.3251,4.0,'NA',2.580)
+myFunction(6.551,0.3251,4.0,NA,2.580)
