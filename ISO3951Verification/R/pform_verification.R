9b82ad6c5688118bdc6f845ad6248dbceefb8a2c
de019050 <patric.stracke@sanofi.com>
2024-01-30 17:31:53 +0100

    implemented P function

--- ISO3951Verification/R/pform_verification.R
+++ ISO3951Verification/R/pform_verification.R
@@ -7,7 +7,7 @@ setwd(getwd())
 # Allreflex Data until 25012024
 ALLREFLEX <- read_csv("DATA/ALLREFLEX.csv")
 
-# Kform Examples from Chapter 16.2 of ISO 3951-2
+# pform Examples from Chapter 16.3.2 of ISO 3951-2
 ISO_PFORMTest <- read_csv("DATA/ISO_PFORMTest.csv")
 
 
@@ -17,94 +17,6 @@ ISO_PFORMTest <- read_csv("DATA/ISO_PFORMTest.csv")
 ISO_PFORM_Input<-ISO_PFORMTest %>%
   group_by(Charge,LSL,USL,pValue,fsFac) %>%
   summarise(n=n(),value=mean(Value,na.rm=T), std=sd(Value,na.rm=T))
-
-
-
-# New MSSD function
-# bases on tibble containing all neccessary informations
-# input only one tibble
-
-MSSD <-function(data){
-
-
-  # actuation
-  mssd_act=ifelse((data$act_usl-data$act_lsl)*data$fsfac-data$sact<0,"FALSE","TRUE")
-  mssd_act_val=(data$act_usl-data$act_lsl)*data$fsfac-data$sact
-  # itime
-  #mssd_itime=
-  # idepth
-  mssd_idepth=ifelse((data$idepth_usl-data$idepth_lsl)*data$fsfac-data$sidepth<0,"FALSE","TRUE")
-  mssd_idepth_val=(data$idepth_usl-data$idepth_lsl)*data$fsfac-data$sidepth
-  # dose
-  #mssd_dose=
-  # npos
-  #mssd_npos=
-
-  #return=cbind(mssd_act,mssd_itime,mssd_idepth,mssd_dose,mssd_npos)
-  return=cbind(data$charge,data$mat_bez,mssd_act,mssd_act_val,mssd_idepth,mssd_idepth_val,data$fsfac)
-
-}
-
-
-
-p_hat <- function(data){
-  n<-data$newn
-  # actuation
-  q_l_act = (data$mact-data$act_lsl)/data$sact
-  x_l_act = 0.5*(1-(q_l_act*sqrt(n))/(n-1))
-  p_hat_l_act = pbeta(as.numeric(x_l_act),(n-2)/2,(n-2)/2)
-
-  q_u_act=(data$act_usl-data$mact)/data$sact
-  x_u_act=0.5*(1-(as.numeric(q_u_act)*sqrt(n))/(n-1))
-  p_hat_u_act=pbeta(x_u_act,(n-2)/2,(n-2)/2)
-  p_hat_act_val=p_hat_l_act+p_hat_u_act
-  p_hat_act=ifelse(p_hat_act_val-data$pfac>0,"FALSE","TRUE")
-  # itime
-  # q_l_itime=(data$m_itime-data_2[1,4])/data$sd_itime
-  # x_l_itime=0.5*(1-(q_l_itime*sqrt(n))/(n-1))
-  # p_hat_l_itime=pbeta(x_l_itime,(n-2)/2,(n-2)/2)
-  # q_u_itime=(data_2[1,5]-data$m_itime)/data$sd_itime
-  # x_u_itime=0.5*(1-(q_u_itime*sqrt(n))/(n-1))
-  # p_hat_u_itime=pbeta(x_u_itime,(n-2)/2,(n-2)/2)
-  # p_hat_itime=p_hat_l_itime+p_hat_u_itime
-  # idepth
-  q_l_idepth=(data$midepth-data$idepth_lsl)/data$sidepth
-  x_l_idepth=0.5*(1-(as.numeric(q_l_idepth)*sqrt(n))/(n-1))
-  p_hat_l_idepth=pbeta(x_l_idepth,(n-2)/2,(n-2)/2)
-  q_u_idepth=(data$idepth_usl-data$midepth)/data$sidepth
-  x_u_idepth=0.5*(1-(as.numeric(q_u_idepth)*sqrt(n))/(n-1))
-  p_hat_u_idepth=pbeta(x_u_idepth,(n-2)/2,(n-2)/2)
-  p_hat_idepth_val=p_hat_l_idepth+p_hat_u_idepth
-  p_hat_idepth=ifelse(p_hat_idepth_val-data$pfac>0,"FALSE","TRUE")
-
-  # dose
-  # q_l_dose=(data$m_dose-data_2[1,8])/data$sd_dose
-  # x_l_dose=0.5*(1-(q_l_dose*sqrt(n))/(n-1))
-  # p_hat_l_dose=pbeta(x_l_dose,(n-2)/2,(n-2)/2)
-  # q_u_dose=(data_2[1,9]-data$m_dose)/data$sd_dose
-  # x_u_dose=0.5*(1-(q_u_dose*sqrt(n))/(n-1))
-  # p_hat_u_dose=pbeta(x_u_dose,(n-2)/2,(n-2)/2)
-  # p_hat_dose=p_hat_l_dose+p_hat_u_dose
-
-  # npos
-  # q_l_npos=(data$m_npos-data_2[1,10])/data$sd_npos
-  # x_l_npos=0.5*(1-(q_l_npos*sqrt(n))/(n-1))
-  # p_hat_l_npos=pbeta(x_l_npos,(n-2)/2,(n-2)/2)
-  # q_u_npos=(data_2[1,11]-data$m_npos)/data$sd_npos
-  # x_u_npos=0.5*(1-(q_u_npos*sqrt(n))/(n-1))
-  # p_hat_u_npos=pbeta(x_u_npos,(n-2)/2,(n-2)/2)
-  # p_hat_npos=p_hat_l_npos+p_hat_u_npos
-
-
-  #return=cbind(p_hat_act,p_hat_itime,p_hat_idepth,p_hat_dose,p_hat_npos)
-  return=cbind(data$charge,data$mat_bez,p_hat_act_val,p_hat_act,p_hat_idepth_val,p_hat_idepth,data$pfac)
-
-
-
-}
-
-
-
 
 
 # My new Verification Pform function
@@ -133,8 +45,25 @@ myPFunction <- function(input1, input2, input3, input4, input5, input6, input7) 
   # Example operation: sum of all inputs
   if (STD>(USL-LSL)*fsFac){
     cat ('Lot can already be rejected,  STD = ',STD,' is > than SMAX =',round((USL-LSL)*fsFac,digits=3),' But check 2. calculation!\n')
-  #}
-  result <- sum(unlist(inputs))
+    q_l = (MeanValue-LSL)/STD
+    x_l = 0.5*(1-(q_l*sqrt(n))/(n-1))
+    p_hat_l = pbeta(as.numeric(x_l),(n-2)/2,(n-2)/2)
+
+    cat ('q_l,x_l, p_hat_l',q_l, x_l, p_hat_l, '\n')
+    q_u=(USL-MeanValue)/STD
+    x_u=0.5*(1-(as.numeric(q_u)*sqrt(n))/(n-1))
+    p_hat_u=pbeta(x_u,(n-2)/2,(n-2)/2)
+    cat ('q_u,x_u, p_hat_u',q_u, x_u, p_hat_u, '\n')
+    p_hat_val=p_hat_l+p_hat_u
+
+
+    if (round(p_hat_val,digit=6) <= pFac){
+      cat('The Lot meets the acceptability criterion.criterion. p^ = ',round(p_hat_val,digit=6),' is leq than p* =',pFac,'! Lot can be accepted!\n')
+    } else if (round(p_hat_val,digit=6) > pFac){
+      cat('The Lot does not meet the acceptability criterion. p^ = ',round(p_hat_val,digit=6),' is > than p* =',pFac,' ! Lot shall be rejected\n')
+    }
+  }
+  #result <- sum(unlist(inputs))
   #return(result)
 
   q_l = (MeanValue-LSL)/STD
@@ -146,16 +75,24 @@ myPFunction <- function(input1, input2, input3, input4, input5, input6, input7) 
   x_u=0.5*(1-(as.numeric(q_u)*sqrt(n))/(n-1))
   p_hat_u=pbeta(x_u,(n-2)/2,(n-2)/2)
   cat ('q_u,x_u, p_hat_u',q_u, x_u, p_hat_u, '\n')
-
-
   p_hat_val=p_hat_l+p_hat_u
 
-  cat ('Lot shall be rejected,  p^ = ',round(p_hat_val,digit=6),' is > than p* =',pFac,'!')
+  if (round(p_hat_val,digit=6) <= pFac){
+    cat('The Lot meets the acceptability criterion.criterion. p^ = ',round(p_hat_val,digit=6),' is leq than p* =',pFac,'! Lot can be accepted!\n')
+  } else if (round(p_hat_val,digit=6) > pFac){
+    cat('The Lot does not meet the acceptability criterion. p^ = ',round(p_hat_val,digit=6),' is > than p* =',pFac,' ! Lot shall be rejected!\n')
   }
-}
 
+}
+ISO_PFORM_Input
 # Example usage
 #myPFunction(MeanValue,STD,LSL,USL,pFac,fsFac,n)
+# Example Chapter 16.3.2.2
 myPFunction(3.5, 7.436,-10 , 10, 0.1925, 0.475, 3)
-myPFunction(1,2,3,4,5,6,7)
+# Example Chapter 16.3.2.3
+myPFunction(82.5,0.4082,82,84,0.0860,0.365,4)
 
+# Example Chapter 16.3.2.4
+myPFunction(64.223,2.7899,60,70,0.051590,0.274,13)
+# Example Chapter 16.3.2.5
+myPFunction(64.223,2.7899,60,70,0.06466,0.285,13)
