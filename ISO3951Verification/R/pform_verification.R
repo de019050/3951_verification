0813a1c7e7da7c0d814343b8e0b6f505783ecbd7
de019050 <patric.stracke@sanofi.com>
2024-02-01 16:50:18 +0100

    Update pform_verification.R
    added examples

--- ISO3951Verification/R/pform_verification.R
+++ ISO3951Verification/R/pform_verification.R
@@ -17,6 +17,16 @@
 #'
 #' @examples
 #'
+#'
+#' #  Example usage
+#' #myPFunction(MeanValue,STD,LSL,USL,pFac,fsFac,n)
+#'
+#' # Example Chapter 16.3.2.2
+#'  myPFunction(3.5, 7.436,-10 , 10, 0.1925, 0.475, 3)
+#'
+#' # Example Chapter 16.3.2.3
+#' myPFunction(82.5,0.4082,82,84,0.0860,0.365,4)
+#'
 #' # Example ISO 3951-2 Chapter 16.3.2.4
 #'  myPFunction(64.223,2.7899,60,70,0.051590,0.274,13)
 #'
