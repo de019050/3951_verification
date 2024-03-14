b5cb8070abc63dd7eca6582be1649d5c9eea9a9d
de019050 <patric.stracke@sanofi.com>
2024-03-14 17:44:28 +0100

    added mode selection

--- ISO3951Verification/R/kform_verification.R
+++ ISO3951Verification/R/kform_verification.R
@@ -1,80 +1,69 @@
-
-
-# Define the function with five parameters
-#' k-Form function (s-Method only)
+#' k-Form function (s-Method only, with mode selection)
 #'
 #' @param input1 Mean value
 #' @param input2 Standard deviation
 #' @param input3 LSL lower specification limit
 #' @param input4 USL upper specification limit
-#' @param input5 kFac,  tabled acceptance criterion
+#' @param input5 kFac, tabled acceptance criterion
+#' @param input6 Mode of operation, must be either "adhoc" or "batch"
 #'
-#' @return Result String
-#' @export (readr, stats,dplry )
+#' @return Depending on the mode, returns a detailed string for "adhoc" or a numeric value for "batch"
+#' @export
 #'
 #' @examples
 #'
-#'
-#'
-#' #  Example usage
-#' #myPFunction(MeanValue,STD,LSL,USL,kFac)
+#' #' #  Example usage
+#' #mykFunction(MeanValue,STD,LSL,USL,kFac,mode)
 #'
 #' # Example ISO 3951-2 Chapter 16.2
-#'  myKFunction(6.551,0.3251,4.0,NA,2.580)
+#'  myKFunction(6.551,0.3251,4.0,NA,2.580,'batch')
+#'  myKFunction(6.551,0.3251,4.0,NA,2.580,'adhoc')
 #'
 #' # Example ISO 3951-2 Chapter 16.2
-#'  myKFunction(54.615385,3.3301267,NA,60,1.426)
+#'  myKFunction(54.615385,3.3301267,NA,60,1.426,'batch')
+#'  myKFunction(54.615385,3.3301267,NA,60,1.426,'adhoc')
 #'
 #'
-#'
-myKFunction <- function(input1, input2, input3 = NULL, input4 = NULL, input5) {
-  MeanValue<-input1
-  STD<-input2
-  LSL<-input3
-  USL<-input4
-  kFac<-input5
+myKFunction <- function(input1, input2, input3 = NULL, input4 = NULL, input5, input6) {
+  MeanValue <- input1
+  STD <- input2
+  LSL <- input3
+  USL <- input4
+  kFac <- input5
+  mode <- tolower(input6) # Ensure mode is in lower case
 
-    # Check if mandatory inputs are missing or NA
-  if (missing(input1) || is.na(input1) || missing(input2) || is.na(input2) || missing(input5) || is.na(input5)) {
-    stop("Mean Value, Standard Deviation, and k-Factor are mandatory and cannot be NA.")
+  # Check if mandatory inputs are missing or NA, including the new mandatory 'input6'
+  if (missing(input1) || is.na(input1) ||
+      missing(input2) || is.na(input2) ||
+      missing(input5) || is.na(input5) ||
+      missing(input6) || !input6 %in% c("adhoc", "batch")) {
+    stop("Mean Value, Standard Deviation, k-Factor, and Mode ('adhoc' or 'batch') are mandatory and cannot be NA or invalid.")
   }
 
-  # Determine which option to use based on the presence and value of optional inputs
-  input3_present = !missing(input3) && !is.na(input3)
-  input4_present = !missing(input4) && !is.na(input4)
+  input3_present <- !missing(input3) && !is.na(input3)
+  input4_present <- !missing(input4) && !is.na(input4)
 
-  # Check if input4 is larger than input3 when both are present
   if (input3_present && input4_present && input4 <= input3) {
-      print(input3)
-      print(input4)
-      stop("USL must be larger than LSL.")
+    stop("USL must be larger than LSL.")
   }
 
   if (!input3_present && !input4_present) {
     stop("At least one of LSL or USL is required.")
-  } else if (input4_present && !input3_present) {
-    QU= (USL-MeanValue)/STD
-    #print(round(QU,digits=3))
-    if (QU >= kFac){
-      cat('The Lot meets the acceptability criterion. QU =',round(QU,digits=3),'is geq than kfac =',kFac,'\n')
-    } else if (QU < kFac){
-      cat('The Lot does not meet the acceptability criterion. QU =',round(QU,digits=3),'is less than kfac =',kFac,'\n')
-    } else {
-      cat('QU Check inputs')
-    }
-  } else if (input3_present && !input4_present) {
-    QL= (MeanValue-LSL)/STD
-    #print(round(QL,digits = 3))
-    if (QL >= kFac){
-      cat('The Lot meets the acceptability criterion. QL =',round(QL,digits=3),'is geq than kfac =',kFac,'\n')
-    } else if (QL < kFac){
-      cat('The Lot does not meet the acceptability criterion. QL =',round(QL,digits=3),'is less than kfac =',kFac,'\n')
-    } else {
-      cat('Check inputs')
+  } else {
+    if (input4_present && !input3_present) {
+      QU <- (USL - MeanValue) / STD
+      if (mode == "batch") {
+        return(round(QU, digits = 3))
       }
+      cat('The Lot meets the acceptability criterion. QU =', round(QU, digits = 3), 'is geq than kfac =', kFac, '\n')
+    } else if (input3_present && !input4_present) {
+      QL <- (MeanValue - LSL) / STD
+      if (mode == "batch") {
+        return(round(QL, digits = 3))
+      }
+      cat('The Lot meets the acceptability criterion. QL =', round(QL, digits = 3), 'is geq than kfac =', kFac, '\n')
     } else {
-    # Proceed with option 3
-    print("Two limits! Use p-form!")
+      print("Two limits! Use p-form!")
+    }
   }
 }
-
--- ISO3951Verification/R/pform_verification.R
+++ ISO3951Verification/R/pform_verification.R
@@ -1,114 +1,83 @@
-
-
-# My new Verification Pform function
-# Define the function with seven parameters
-#' p-Form function (s-Method only)
+#' p-Form function (s-Method only, with mode selection)
 #'
 #' @param input1 Mean value
 #' @param input2 Standard deviation
 #' @param input3 LSL lower specification limit
 #' @param input4 USL upper specification limit
-#' @param input5 p*,  tabled acceptance criterion
+#' @param input5 p*, tabled acceptance criterion
 #' @param input6 fs, tabled factor to calculate S_max
 #' @param input7 n, sample size
+#' @param input8 Mode of operation, must be either "adhoc" or "batch"
 #'
-#' @return Result String
-#' @export (readr, stats,dplry )
+#' @return Depending on the mode, returns a detailed string for "adhoc" or numeric values for "batch"
+#' @export
 #'
 #' @examples
 #'
 #'
-#' #  Example usage
-#' #myPFunction(MeanValue,STD,LSL,USL,pFac,fsFac,n)
 #'
+#' # Example usage
+#' #myPFunction(MeanValue,STD,LSL,USL,pFac,fsFac,n,"mode")
 #' # Example Chapter 16.3.2.2
-#'  myPFunction(3.5, 7.436,-10 , 10, 0.1925, 0.475, 3)
-#'
+#' myPFunction(3.5, 7.436,-10 , 10, 0.1925, 0.475, 3, "adhoc")
+#' myPFunction(3.5, 7.436,-10 , 10, 0.1925, 0.475, 3, "batch")
 #' # Example Chapter 16.3.2.3
-#' myPFunction(82.5,0.4082,82,84,0.0860,0.365,4)
+#' myPFunction(82.5,0.4082,82,84,0.0860,0.365,4, "adhoc")
+#' myPFunction(82.5,0.4082,82,84,0.0860,0.365,4, "adhoc")
 #'
-#' # Example ISO 3951-2 Chapter 16.3.2.4
-#'  myPFunction(64.223,2.7899,60,70,0.051590,0.274,13)
+#' # Example Chapter 16.3.2.4
+#' myPFunction(64.223,2.7899,60,70,0.051590,0.274,13, "adhoc")
+#' myPFunction(64.223,2.7899,60,70,0.051590,0.274,13, "batch")
+#' # Example Chapter 16.3.2.5
+#' myPFunction(64.223,2.7899,60,70,0.06466,0.285,13, "adhoc")
+#' myPFunction(64.223,2.7899,60,70,0.06466,0.285,13, "batch")
 #'
-#' # Example ISO 3951-2 Chapter 16.3.2.5
-#'  myPFunction(64.223,2.7899,60,70,0.051590,0.285,13)
-#'
-#'
-myPFunction <- function(input1, input2, input3, input4, input5, input6, input7) {
-  MeanValue<-input1
-  STD<-input2
-  LSL<-input3
-  USL<-input4
-  pFac<-input5
-  fsFac<-input6
-  n<- input7
+myPFunction <- function(input1, input2, input3, input4, input5, input6, input7, input8) {
+  MeanValue <- input1
+  STD <- input2
+  LSL <- input3
+  USL <- input4
+  pFac <- input5
+  fsFac <- input6
+  n <- input7
+  mode <- tolower(input8) # Ensure mode is in lower case
 
-  # Check if any inputs are missing, NA, or not numeric
-  inputs <- list(input1, input2, input3, input4, input5, input6, input7)
-  if (any(sapply(inputs, is.na)) || any(sapply(inputs, is.null)) || !all(sapply(inputs, is.numeric))) {
-    stop("All inputs must be provided, non-NA, and numeric.")
+  # Check if any inputs are missing, NA, or not numeric, and if mode is valid
+  if (any(sapply(list(input1, input2, input3, input4, input5, input6, input7), is.na)) ||
+      !all(sapply(list(input1, input2, input3, input4, input5, input6, input7), is.numeric)) ||
+      !input8 %in% c("adhoc", "batch")) {
+    stop("All inputs must be provided, non-NA, numeric, and mode must be either 'adhoc' or 'batch'.")
   }
 
-  # Check if input4 is larger than input3
   if (USL <= LSL) {
     stop("USL must be larger than LSL.")
   }
 
-  # Perform the operation
-  # Example operation: sum of all inputs
-  if (STD>(USL-LSL)*fsFac){
-    cat ('Lot can already be rejected,  STD = ',STD,' is > than SMAX =',round((USL-LSL)*fsFac,digits=3),' But check 2. calculation!\n')
-    q_l = (MeanValue-LSL)/STD
-    x_l = 0.5*(1-(q_l*sqrt(n))/(n-1))
-    p_hat_l = pbeta(as.numeric(x_l),(n-2)/2,(n-2)/2)
+  # Calculate q_l, x_l, p_hat_l, q_u, x_u, p_hat_u
+  q_l <- (MeanValue - LSL) / STD
+  x_l <- 0.5 * (1 - (q_l * sqrt(n)) / (n - 1))
+  p_hat_l <- pbeta(x_l, (n-2)/2, (n-2)/2)
 
-    cat ('q_l,x_l, p_hat_l',q_l, x_l, p_hat_l, '\n')
-    q_u=(USL-MeanValue)/STD
-    x_u=0.5*(1-(as.numeric(q_u)*sqrt(n))/(n-1))
-    p_hat_u=pbeta(x_u,(n-2)/2,(n-2)/2)
-    cat ('q_u,x_u, p_hat_u',q_u, x_u, p_hat_u, '\n')
-    p_hat_val=p_hat_l+p_hat_u
+  q_u <- (USL - MeanValue) / STD
+  x_u <- 0.5 * (1 - (q_u * sqrt(n)) / (n - 1))
+  p_hat_u <- pbeta(x_u, (n-2)/2, (n-2)/2)
 
+  p_hat_val <- p_hat_l + p_hat_u
 
-    if (round(p_hat_val,digit=6) <= pFac){
-      cat('The Lot meets the acceptability criterion.criterion. p^ = ',round(p_hat_val,digit=6),' is leq than p* =',pFac,'! Lot can be accepted!\n')
-    } else if (round(p_hat_val,digit=6) > pFac){
-      cat('The Lot does not meet the acceptability criterion. p^ = ',round(p_hat_val,digit=6),' is > than p* =',pFac,' ! Lot shall be rejected\n')
+  if (mode == "batch") {
+    return(list(STD = (USL-LSL) * fsFac, p_hat_val = round(p_hat_val,digits=6)))
+  } else {
+    if (STD > (USL-LSL) * fsFac) {
+      cat(sprintf("Lot can already be rejected, STD = %f is > than SMAX = %f. But check 2nd calculation!\n", STD, round((USL-LSL)*fsFac, digits=3)))
+    }
+
+    cat(sprintf("q_l = %f, x_l = %f, p_hat_l = %f\n", q_l, x_l, p_hat_l))
+    cat(sprintf("q_u = %f, x_u = %f, p_hat_u = %f\n", q_u, x_u, p_hat_u))
+
+    if (round(p_hat_val, digits=6) <= pFac) {
+      cat(sprintf("The Lot meets the acceptability criterion. p^ = %f is leq than p* = %f! Lot can be accepted!\n", round(p_hat_val, digits=6), pFac))
+    } else {
+      cat(sprintf("The Lot does not meet the acceptability criterion. p^ = %f is > than p* = %f! Lot shall be rejected\n", round(p_hat_val, digits=6), pFac))
     }
   }
-  #result <- sum(unlist(inputs))
-  #return(result)
-
-  q_l = (MeanValue-LSL)/STD
-  x_l = 0.5*(1-(q_l*sqrt(n))/(n-1))
-  p_hat_l = pbeta(as.numeric(x_l),(n-2)/2,(n-2)/2)
-
-  cat ('q_l,x_l, p_hat_l',q_l, x_l, p_hat_l, '\n')
-  q_u=(USL-MeanValue)/STD
-  x_u=0.5*(1-(as.numeric(q_u)*sqrt(n))/(n-1))
-  p_hat_u=pbeta(x_u,(n-2)/2,(n-2)/2)
-  cat ('q_u,x_u, p_hat_u',q_u, x_u, p_hat_u, '\n')
-  p_hat_val=p_hat_l+p_hat_u
-
-  if (round(p_hat_val,digit=6) <= pFac){
-    cat('The Lot meets the acceptability criterion.criterion. p^ = ',round(p_hat_val,digit=6),' is leq than p* =',pFac,'! Lot can be accepted!\n')
-  } else if (round(p_hat_val,digit=6) > pFac){
-    cat('The Lot does not meet the acceptability criterion. p^ = ',round(p_hat_val,digit=6),' is > than p* =',pFac,' ! Lot shall be rejected!\n')
-  }
-
 }
-
-# # Input Data from  examples in Chapter 16.3.2
-#
-# ISO_PFORM_Input
-# # Example usage
-# #myPFunction(MeanValue,STD,LSL,USL,pFac,fsFac,n)
-# # Example Chapter 16.3.2.2
-# myPFunction(3.5, 7.436,-10 , 10, 0.1925, 0.475, 3)
-# # Example Chapter 16.3.2.3
-# myPFunction(82.5,0.4082,82,84,0.0860,0.365,4)
-#
-# # Example Chapter 16.3.2.4
-# myPFunction(64.223,2.7899,60,70,0.051590,0.274,13)
-# # Example Chapter 16.3.2.5
-# myPFunction(64.223,2.7899,60,70,0.06466,0.285,13)
